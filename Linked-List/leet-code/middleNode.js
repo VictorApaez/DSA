64dc9f6dedebfdaa684101e588d09ddd9ad80a70
Victor Apaez <vic.apaez@gmail.com>
2023-02-01 22:05:08 -0500

    206 Reversed Linked List

--- Linked-List/leet-code/middleNode.js
+++ Linked-List/leet-code/middleNode.js
@@ -17,7 +17,3 @@ var middleNode = function (head) {
 
 let testList = new LinkedList(1, 2, 3, 4, 6); // Output: [3,4,5]
 console.log(middleNode(testList.head));
-
-// Input: head = [1,2,3,4,5,6]
-// Output: [4,5,6]
-// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
