18c528795489a6824ce5a390037cae702a7f0cc0
Victor Apaez <vic.apaez@gmail.com>
2023-02-04 23:27:43 -0500

    added isPalindrome to LinkedList class

--- Linked-List/main.js
+++ Linked-List/main.js
@@ -2,9 +2,14 @@ const middleNode = require("./leet-code/middleNode");
 const { LinkedList } = require("./LinkedListClass");
 const reverseList = require("./leet-code/reverseList");
 const deleteSortedDuplicates = require("./leet-code/deleteSortedDuplicates");
+const isPalindrome = require("./leet-code/isPalindrome");
+isPalindrome;
 
 LinkedList.prototype.reverseList = reverseList;
 LinkedList.prototype.middleNode = middleNode;
 LinkedList.prototype.deleteSortedDuplicates = deleteSortedDuplicates;
+LinkedList.prototype.isPalindrome = isPalindrome;
+
+let testing = new LinkedList(1, 1, 2, 3);
 
 module.exports = LinkedList;
