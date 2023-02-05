7ce787616cb326d236698858349e49d4118ef812
Victor Apaez <vic.apaez@gmail.com>
2023-02-04 22:52:17 -0500

    imported and added deleteSortedDuplicates to LinkedList class

--- Linked-List/main.js
+++ Linked-List/main.js
@@ -1,8 +1,10 @@
 const middleNode = require("./leet-code/middleNode");
 const { LinkedList } = require("./LinkedListClass");
 const reverseList = require("./leet-code/reverseList");
+const deleteSortedDuplicates = require("./leet-code/deleteSortedDuplicates");
 
 LinkedList.prototype.reverseList = reverseList;
 LinkedList.prototype.middleNode = middleNode;
+LinkedList.prototype.deleteSortedDuplicates = deleteSortedDuplicates;
 
 module.exports = LinkedList;
