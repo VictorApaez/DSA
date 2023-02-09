1cca24bed73a99ca8ddcbcdcb8c15727912f82bf
Victor Apaez <vic.apaez@gmail.com>
2023-02-09 13:22:41 -0500

    removed instance

--- Linked-List/main.js
+++ Linked-List/main.js
@@ -1,5 +1,5 @@
-const middleNode = require("./leet-code/middleNode");
 const { LinkedList } = require("./LinkedListClass");
+const middleNode = require("./leet-code/middleNode");
 const reverseList = require("./leet-code/reverseList");
 const deleteSortedDuplicates = require("./leet-code/deleteSortedDuplicates");
 const isPalindrome = require("./leet-code/isPalindrome");
@@ -9,7 +9,5 @@ LinkedList.prototype.reverseList = reverseList;
 LinkedList.prototype.middleNode = middleNode;
 LinkedList.prototype.deleteSortedDuplicates = deleteSortedDuplicates;
 LinkedList.prototype.isPalindrome = isPalindrome;
-
-let testing = new LinkedList(1, 1, 2, 3);
 
 module.exports = LinkedList;
