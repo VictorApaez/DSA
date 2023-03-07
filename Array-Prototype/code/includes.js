5395efe1a8d849aede5fe1cbc5bbb5fa4e832704
VictorApaez <vic.apaez@gmail.com>
2023-03-06 19:28:45 -0500

    created array includes example

---
+++ Array-Prototype/code/includes.js
@@ -0,0 +1,10 @@
+// returns true if the array contains the value, and false if it does not.
+
+// Accepts two arguments:
+// 1. value
+// 2. starting index (optional)
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+arr.includes(3); // true
+arr.includes(3, 3); // false
